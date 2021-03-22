c0244a760ac3b82eb9ead0966926c38b2f294a03
Audrius Tamašauskas <s031158@ad.viko.lt>
2021-03-22 17:58:22 +0200

    Commit 1

---
+++ PraktinisDarbas_AudriusTamasauskasPI19S/PraktinisDarbas_AudriusTamasauskasPI19S/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PraktinisDarbas_AudriusTamasauskasPI19S
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.txtLonInName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(66, 113);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(138, 54);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "Prisijungti";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.Location = new System.Drawing.Point(66, 189);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnLogOut.Size = new System.Drawing.Size(138, 54);
+            this.btnLogOut.TabIndex = 1;
+            this.btnLogOut.Text = "Atsijungti";
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            // 
+            // txtLonInName
+            // 
+            this.txtLonInName.Location = new System.Drawing.Point(39, 20);
+            this.txtLonInName.Name = "txtLonInName";
+            this.txtLonInName.Size = new System.Drawing.Size(195, 22);
+            this.txtLonInName.TabIndex = 2;
+            this.txtLonInName.Text = "Prisijungimo vardas";
+            this.txtLonInName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(39, 70);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(195, 22);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.Text = "Slaptažodis";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::PraktinisDarbas_AudriusTamasauskasPI19S.Properties.Resources.explosion_pink_blue_powder_freeze_260nw_1077184466;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 262);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 267);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLonInName);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Prisijungimas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.TextBox txtLonInName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
